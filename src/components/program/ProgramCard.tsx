88d3ed05ee202cce49418d987072fa61229fdd0d
Shigurex <yamatohokari@gmail.com>
2024-09-13 00:58:14 +0900

    sample design added

--- src/components/program/ProgramCard.tsx
+++ src/components/program/ProgramCard.tsx
@@ -1,5 +1,12 @@
-import Image from "next/image"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card"
+import Image from "next/image";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "../ui/card";
 
 export const ProgramCard = () => {
 	return (
@@ -7,7 +14,13 @@ export const ProgramCard = () => {
 			<CardContent>
 				<div className="bg-white text-blue-500 rounded-lg p-4 flex items-center space-x-4">
 					<div className="bg-blue-500 rounded-full p-2">
-						<Image src="/placeholder.svg" alt="Program icon" width={40} height={40} className="rounded-full" />
+						<Image
+							src="/placeholder.svg"
+							alt="Program icon"
+							width={40}
+							height={40}
+							className="rounded-full"
+						/>
 					</div>
 					<div>
 						<h4 className="font-bold">プログラム名</h4>
@@ -16,5 +29,5 @@ export const ProgramCard = () => {
 				</div>
 			</CardContent>
 		</Card>
-	)
-}
+	);
+};
