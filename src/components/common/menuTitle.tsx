73eb0964295f094a718a538afe36b6b03a510b75
Shigurex <yamatohokari@gmail.com>
2024-09-24 19:42:21 +0900

    gap added

--- src/components/common/menuTitle.tsx
+++ src/components/common/menuTitle.tsx
@@ -1,6 +1,6 @@
 export const MenuTitle = () => {
   return (
-    <div className='w-full px-16 py-6'>
+    <div className='w-full'>
       <div className='text-white flex items-center border-b-4 border-black'>
         <span className='font-bold text-xl bg-black py-2 w-[200px] text-center'>
           PROGRAM
--- src/components/home/programs.tsx
+++ src/components/home/programs.tsx
@@ -3,13 +3,13 @@ import { MenuTitle } from '../common/menuTitle'
 
 export const Program = () => {
   return (
-    <>
+    <div className='container mx-auto flex flex-col p-3 gap-5'>
       <MenuTitle />
       <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
         <ProgramCard />
         <ProgramCard />
         <ProgramCard />
       </div>
-    </>
+    </div>
   )
 }
