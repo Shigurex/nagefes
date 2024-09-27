cb72ea537964d3996f534a3a9541d458c78d9286
Shigurex <yamatohokari@gmail.com>
2024-09-27 15:52:17 +0900

    layout modified

--- src/components/home/news.tsx
+++ src/components/home/news.tsx
@@ -5,7 +5,6 @@ export const News = () => {
   return (
     <div className='container mx-auto flex flex-col px-3 py-10 gap-5' id='news'>
       <MenuTitle>News</MenuTitle>
-      {/* <div className='h-[400px] w-full bg-slate-200 rounded-lg' /> */}
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-2'>
         {newsItems.map(news => (
           <div key={news.context} className='p-2'>
