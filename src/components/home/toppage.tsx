cd0a79fd608609d0679fd32238f632ca7384a82c
Shigurex <yamatohokari@gmail.com>
2024-09-27 15:51:47 +0900

    layout modified

--- src/components/home/toppage.tsx
+++ src/components/home/toppage.tsx
@@ -5,26 +5,26 @@ import { Button } from '../ui/button'
 export default function TopPage() {
   return (
     <div
-      className="relative bg-[url('/background.png')] bg-cover h-auto aspect-[4/3]"
+      className="relative bg-[url('/background.png')] bg-cover h-auto aspect-[1/1] sm:aspect-[4/3]"
       id='top'
     >
-      <div className='absolute bottom-0 w-full flex flex-col gap-4 justify-center p-6'>
+      <div className='absolute bottom-0 w-full flex flex-col gap-2 md:gap-4 justify-center p-4 md:p-6'>
         <Link href='/#program' className='flex justify-center'>
-          <div className='justify-center w-96 bg-white border-2 rounded-lg p-3 font-semibold text-black flex items-center space-x-2 shadow-md'>
+          <div className='justify-center w-72 md:w-96 bg-white border-2 rounded-lg p-3 font-semibold text-black flex items-center shadow-md'>
             <span className='text-sm md:text-base'>
               イベント申し込みはこちらから
             </span>
           </div>
         </Link>
         <div className='flex justify-center'>
-          <div className='justify-center p-3 font-semibold text-black flex items-center gap-5'>
-            <span className='text-base md:text-lg'>Sponsored By</span>
+          <div className='justify-center p-3 font-semibold text-black flex items-center gap-3 md:gap-5'>
+            <span className='text-sm md:text-lg'>Sponsored By</span>
             <Image
               src='/logo_nishi.svg'
-              alt='猫は最高に可愛い'
+              alt='Nishi'
               width='500'
               height='500'
-              className='h-8 w-auto'
+              className='h-5 md:h-8 w-auto'
             />
           </div>
         </div>
