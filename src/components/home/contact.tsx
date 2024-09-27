5b0811dc209160e75e3154cca36d0c6cba7dd79f
Shigurex <yamatohokari@gmail.com>
2024-09-27 15:44:22 +0900

    toppage button added

--- src/components/home/contact.tsx
+++ src/components/home/contact.tsx
@@ -7,7 +7,7 @@ export const Contact = () => {
     <div className='flex flex-col px-3 py-10 gap-5' id='contact'>
       <MenuTitle>Contact</MenuTitle>
       <div className='flex flex-col gap-4'>
-        <div className='justify-center w-80 bg-white border-2 border-black rounded-lg p-3 font-semibold text-black flex items-center space-x-2 shadow-md'>
+        <div className='justify-center w-64 md:w-80 bg-white border-2 border-black rounded-lg p-3 font-semibold text-black flex items-center space-x-2 shadow-md'>
           <span className='text-sm md:text-base'>協賛・取材のお問い合わせ</span>
         </div>
         <div className='flex flex-col gap-3'>
@@ -26,7 +26,7 @@ export const Contact = () => {
             </span>
           </div>
         </div>
-        <div className='justify-center w-80 bg-white border-2 border-black rounded-lg p-3 font-semibold text-black flex items-center space-x-2 shadow-md'>
+        <div className='justify-center w-64 md:w-80 bg-white border-2 border-black rounded-lg p-3 font-semibold text-black flex items-center space-x-2 shadow-md'>
           <span className='text-sm md:text-base'>その他のお問い合わせ</span>
         </div>
         <div className='flex flex-col gap-3'>
@@ -37,7 +37,7 @@ export const Contact = () => {
             </div>
           </Link>
         </div>
-        <div className='justify-center w-80 bg-white border-2 border-black rounded-lg p-3 font-semibold text-black flex items-center space-x-2 shadow-md'>
+        <div className='justify-center w-64 md:w-80 bg-white border-2 border-black rounded-lg p-3 font-semibold text-black flex items-center space-x-2 shadow-md'>
           <span className='text-sm md:text-base'>大会運営責任者</span>
         </div>
       </div>
--- src/components/home/toppage.tsx
+++ src/components/home/toppage.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import { Button } from '../ui/button'
 
 export default function TopPage() {
@@ -6,16 +8,27 @@ export default function TopPage() {
       className="relative bg-[url('/background.png')] bg-cover h-auto aspect-[4/3]"
       id='top'
     >
-      {/* <div className='w-full flex justify-center'>
-        <Button className='text-md p-4'>イベント申し込みはこちら！</Button>
-      </div> */}
-      {/* <div className='w-full'>
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
-          <ProgramCard />
-          <ProgramCard />
-          <ProgramCard />
+      <div className='absolute bottom-0 w-full flex flex-col gap-4 justify-center p-6'>
+        <Link href='/#program' className='flex justify-center'>
+          <div className='justify-center w-96 bg-white border-2 rounded-lg p-3 font-semibold text-black flex items-center space-x-2 shadow-md'>
+            <span className='text-sm md:text-base'>
+              イベント申し込みはこちらから
+            </span>
+          </div>
+        </Link>
+        <div className='flex justify-center'>
+          <div className='justify-center p-3 font-semibold text-black flex items-center gap-5'>
+            <span className='text-base md:text-lg'>Sponsored By</span>
+            <Image
+              src='/logo_nishi.svg'
+              alt='猫は最高に可愛い'
+              width='500'
+              height='500'
+              className='h-8 w-auto'
+            />
+          </div>
         </div>
-      </div> */}
+      </div>
     </div>
   )
 }
